8b04c20f79731980d87e968eddf0b89831ccb57b
TkToshi <k_y_t_active@yahoo.co.jp>
2024-05-07 20:44:41 +0900

    userモデルでバリデーション記述(ユーザー登録)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :read_last_name, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :read_first_name, presence: true, format: { with: VALID_KANA_REGEX }
 end
