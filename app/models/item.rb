548038704b70d82e8bc9aaf1e1da7fe4573e8345
TkToshi <k_y_t_active@yahoo.co.jp>
2024-05-09 15:25:36 +0900

    バリデーションの設定（商品出品機能の実装）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one    :order
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :item_description, presence: true
+  validates :item_price, presence: true
+
 end
